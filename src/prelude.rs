00fe4607525a22c86de6b1081664405ac8d9c8f4
Jose Melo <jmelo@live.cl>
2023-11-29 12:40:54 +0200

    rename mixed curve for composite

--- src/prelude.rs
+++ src/prelude.rs
@@ -29,7 +29,7 @@ pub use crate::{
         },
         traits::*,
         yieldtermstructure::{
-            discounttermstructure::*, flatforwardtermstructure::*, mixedtermstructure::*,
+            compositetermstructure::*, discounttermstructure::*, flatforwardtermstructure::*,
             tenorbasedzeroratetermstructure::*, traits::*, zeroratetermstructure::*,
         },
     },
