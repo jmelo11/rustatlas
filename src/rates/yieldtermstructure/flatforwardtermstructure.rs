596a52e13d6a14239ffb0451d071bc251a1c674a
Jose Melo <jmelo@live.cl>
2024-03-30 23:41:24 -0300

    return rate_definition as copy

--- src/rates/yieldtermstructure/flatforwardtermstructure.rs
+++ src/rates/yieldtermstructure/flatforwardtermstructure.rs
@@ -48,7 +48,7 @@ impl FlatForwardTermStructure {
     }
 
     pub fn rate_definition(&self) -> RateDefinition {
-        *self.rate.rate_definition()
+        self.rate.rate_definition()
     }
 }
 
