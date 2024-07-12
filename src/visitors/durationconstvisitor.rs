0d70eb8dcf2f5e3d50de78d2efd0cb68aefa95e4
Carlos V <carlos.villarroel@itau.cl>
2024-07-12 14:45:35 -0400

    simplificacion visitante duracion

--- src/visitors/durationconstvisitor.rs
+++ src/visitors/durationconstvisitor.rs
@@ -48,10 +48,7 @@ impl<'a, T: HasCashflows> ConstVisit<T> for DurationConstVisitor<'a> {
 
                 let df = cf_market_data.df()?;
                 let fx = cf_market_data.fx()?;
-                let flag = match cf.side() {
-                    Side::Pay => -1.0,
-                    Side::Receive => 1.0,
-                };
+                let flag = cf.side().sign();
 
                 let aux_amount = cf.amount()? * df / fx * flag;
 
